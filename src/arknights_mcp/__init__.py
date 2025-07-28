e09dc75b75f70620326203ff9d0ddd0f281fe898
gjeodnd12165 <princewoong@gmail.com>
2025-07-28 15:44:23 +0900

    ✨ Clone raw data repo

--- src/arknights_mcp/__init__.py
+++ src/arknights_mcp/__init__.py
@@ -3,8 +3,17 @@
 # SPDX-License-Identifier: MIT
 
 from .server import mcp
+from .raw_data import clone_raw_data_repo
+
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
 
 def main():
+    clone_raw_data_repo()
     mcp.run()
 
 if __name__ == "__main__":
